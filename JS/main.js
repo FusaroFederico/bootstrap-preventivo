7a91a0238a22da55668643bdf34f93c6358ec5fd
FedeFus <fusarofede.15@gmail.com>
2024-05-28 10:45:24 +0200

    add: variables inizialization to main.js; some useful id attributes to index.html

--- JS/main.js
+++ JS/main.js
@@ -3,3 +3,16 @@ JS strict mode
 *************/
 'use strict';
 
+// variables inizialization
+const userFirstName = document.getElementById('user-firstname');
+const userLastName = document.getElementById('user-lastname');
+const userEmail = document.getElementById('user-email');
+const userReqWork = document.getElementById('user-reqwork');
+const promCode = document.getElementById('prom-code');
+const privacyCheck = document.getElementById('privacy-check');
+const userForm = document.getElementById('user-form');
+const priceUnit = document.getElementById('price-unit');
+const priceDec = document.getElementById('price-dec');
+
+// array with valid promotional codes
+const valPromCode = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24'];
